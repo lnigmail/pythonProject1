13f3e2d713a7c198285fbb1c799da14a99e1506f
lnigmail <87995463+lnigmail@users.noreply.github.com>
2021-07-28 02:55:42 +0300

    Add files via upload

---
+++ targil1.py
@@ -0,0 +1,6 @@
+name=input("Enter your name: ")
+age=input("Enter your Age: ")
+mail=input("Enter your mail: ")
+
+print("\nFull name: " + name + "\nYour Age is: " + str(age) + "\nYour mail: " + mail)
+
