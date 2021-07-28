05b2c8ee274b63e5033572a92fc350ace515150d
lnigmail <87995463+lnigmail@users.noreply.github.com>
2021-07-28 21:30:09 +0300

    Update targil2.py

--- targil2.py
+++ targil2.py
@@ -7,7 +7,7 @@ Asarot=6
 Ahadot=7
 '''
 
-print("Alafim:" + str(num //1000))
+print("Alafim=" + str(num //1000))
 print("Meot=" + str(num //900))
 print("Asarot=" + str(num //750))
 print("Ahadot=" + str(num //641))
