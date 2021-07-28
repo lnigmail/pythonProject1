314ec35596b765ce74882f8d2d6b0e14dcfec8b3
lnigmail <87995463+lnigmail@users.noreply.github.com>
2021-07-28 21:25:25 +0300

    MATH

---
+++ targil2.py
@@ -0,0 +1,13 @@
+num: int=4567
+
+'''
+Alafim=4
+Meot=5
+Asarot=6
+Ahadot=7
+'''
+
+print("Alafim:" + str(num //1000))
+print("Meot=" + str(num //900))
+print("Asarot=" + str(num //750))
+print("Ahadot=" + str(num //641))
