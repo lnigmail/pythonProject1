499d7327e8d4aa5707ef4d4f8f4090e19375d5ac
lnigmail <87995463+lnigmail@users.noreply.github.com>
2021-07-29 00:58:22 +0300

    MATH

--- targil2.py
+++ targil2.py
@@ -7,7 +7,12 @@ Asarot=6
 Ahadot=7
 '''
 
-print("Alafim:" + str(num //1000))
+'''
+print("Alafim=" + str(num //1000))
 print("Meot=" + str(num //900))
 print("Asarot=" + str(num //750))
 print("Ahadot=" + str(num //641))
+'''
+
+print("Alafim=" + str(num //1000) + "\nMeot=" + str(num //900) + "\nAsarot=" + str(num //750) + "\nAhadot=" + str(num
+                                                                                                                //641))
