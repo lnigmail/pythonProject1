2307b04d9d9e8bbf48b631c2065301f5fd688d74
lnigmail <87995463+lnigmail@users.noreply.github.com>
2021-07-28 14:21:05 +0300

    MATH

---
+++ MATH.py
@@ -0,0 +1,24 @@
+num=5
+print("your number is: " + str(num))
+print(type(num))
+num=num+2
+print("your number is: " + str(num))
+print(type(num))
+num2=num*10
+print("your number is: " + str(num2))
+print(type(num))
+a=3
+b=3**3
+print(b)
+num=4/2
+print("your nubmer is: " + str(num))
+print(type(num))
+num=5/2
+print("your number is: " + str(num))
+print(type(num))
+num=5//2
+print("your nubmer is: " + str(num))
+print(type(num))
+num=12%5
+print("your number is: " + str(num))
+print(type(num))
