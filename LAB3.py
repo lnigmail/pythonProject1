5e5a38ec49ae086488b40466dd8fba31a16623de
lnigmail <87995463+lnigmail@users.noreply.github.com>
2021-07-30 14:56:11 +0300

    STRINGS & CONSOLE OUTPUT

---
+++ LAB3.py
@@ -0,0 +1,9 @@
+a=Leny Sheperd
+b=lni8wallacoil@gmail.com
+c=47
+
+print("Youre full name is: " + a + "\nYour mail is: " + b + "\nYour age is: " + int(c))
+
+print("Youre full name is: " + a[::-1] + "\nYour age is: " + int(c*3))
+
+print("Leny sheperd" in "idan ben dudu yuval shimon yael gal adam shahar yana")
---
+++ market.py
@@ -0,0 +1,16 @@
+#input of the following & calculation of cost (ils):
+print("pricelisting:\n-----------\ntomatoes=3 NIS\ncucumbers=2 NIS\ncola=5 NIS\nchickens=20 NIS\n")
+
+tomatoes=int(input("Enter amount of tomatoes: "))
+cucumbers=int(input("Enter amount of cucumbers: "))
+cola=int(input("Enter amount of cola: "))
+chicken=int(input("Enter amount of chickens: "))
+
+print("Summary of the order:\ntomatoes:" + str(tomatoes) + "\ncucumbers: " + str(cucumbers) + "\ncola:" + str(cola)
++ "\nchickens=" + str(chickens))
+
+
+sum1=tomatoes*3
+sum2=cucambers*2
+sum3=cola*5
+sum4=chickens*20
