fa1f6b54ff0dcf50e768e3f99964017992285e9a
lnigmail <87995463+lnigmail@users.noreply.github.com>
2021-08-01 12:29:39 +0300

    STRINGS & CONSOLE OUTPUT

--- LAB3.py
+++ LAB3.py
@@ -1,9 +1,17 @@
-a=Leny Sheperd
-b=lni8wallacoil@gmail.com
+'''Create variables: string of your name, another string of your mail.
+Creater variable of your age (integer)
+Print all of them to the screen
+
+then print your name from the end to the begining and print it only with your age*3
+Then check if your name is stored inside this full string:
+"idan ben dudu yuval shimon yael gal adam shahar yana"'''
+
+a="Leny"
+b="lni8wallacoil@gmail.com"
 c=47
 
-print("Youre full name is: " + a + "\nYour mail is: " + b + "\nYour age is: " + int(c))
+print("Youre name is:= " + (a) + "\nYour mail is: " + (b) + "\nYour age is: " + str(c))
 
-print("Youre full name is: " + a[::-1] + "\nYour age is: " + int(c*3))
+print("\nYoure name is: " + str(a[::-1]) + "\nYour age is: " + str(c*3))
 
-print("Leny sheperd" in "idan ben dudu yuval shimon yael gal adam shahar yana")
+print("\nLeny" in "idan ben dudu yuval shimon yael gal adam shahar yana")
