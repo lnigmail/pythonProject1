9f10527372254134678d7354d9508550d42f82c8
lnigmail <87995463+lnigmail@users.noreply.github.com>
2021-08-02 22:25:06 +0300

    *

---
+++ targilELIFCONDITION.py
@@ -0,0 +1,45 @@
+#from time import sleep
+
+name=input("Enter your name: \n")
+if(name=="ainshtein"):
+    print("Hello ainshtein genius\n")
+elif(name=="lni"):
+    print("Hello lni lni\n")
+elif(name=="igal"):
+    print("Hello igal cohen\n")
+else:
+    print("Is there a world pandemic? where did everyone go?...\n")
+
+print("It was a nice game, bye bye...\n")
+
+'''
+Enter your name: 
+ainshtein
+Hello ainshtein genius
+
+It was a nice game, bye bye...
+'''
+
+'''
+Enter your name: 
+lni
+Hello lni lni
+
+It was a nice game, bye bye...
+'''
+
+'''
+Enter your name: 
+igal
+Hello igal cohen
+
+It was a nice game, bye bye...
+'''
+
+'''
+Enter your name: 
+alon
+Is there a world pandemic? where did everyone go?...
+
+It was a nice game, bye bye...
+'''
