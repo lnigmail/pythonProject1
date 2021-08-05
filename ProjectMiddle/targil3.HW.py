6a56910d2f66be7948239107b6e3cf5522f48b68
lnigmail <87995463+lnigmail@users.noreply.github.com>
2021-08-05 20:27:09 +0300

    Update targil3.HW.py

--- ProjectMiddle/targil3.HW.py
+++ ProjectMiddle/targil3.HW.py
@@ -1,5 +1,7 @@
 a=input("Enter Full name: ")
 print(' '.join(a[::-1]))
-# ainshtein genius
-# suineg niethsnia
-# s u i n e g   n i e t h s n i a
+
+
+ainshtein genius
+suineg niethsnia
+s u i n e g   n i e t h s n i a
