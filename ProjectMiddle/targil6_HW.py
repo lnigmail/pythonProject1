a1a2d081daa7f7c3d1e7b0d667190f763a213a23
lnigmail <87995463+lnigmail@users.noreply.github.com>
2021-08-06 01:18:56 +0300

    If their is returned number at str(num%2) =the number is ODD

---
+++ ProjectMiddle/targil6_HW.py
@@ -0,0 +1,22 @@
+num=int(input("Enter a number: "))
+if(num%2==0):
+    print("The returned remainder is: " + str(num % 2))
+    print("This number is Even\n")
+else:
+    print("The returned remainder is: " + str(num % 2))
+    print("This number is Odd\n")
+
+print("Alwase a pleasure... Bye Bye...")
+
+# Enter a number: 10
+# The returned remainder is: 0
+# This number is Even
+#
+# Alwase a pleasure... Bye Bye...
+
+# Enter a number: 5
+# The returned remainder is: 1
+# This number is Odd
+#
+# Alwase a pleasure... Bye Bye...
+
