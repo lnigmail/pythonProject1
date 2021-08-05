b2fc84b5646e6b212e89e4e786740ee9694fe0dd
lnigmail <87995463+lnigmail@users.noreply.github.com>
2021-08-05 23:41:22 +0300

    s u i n e g  n i e t h s n i a (' '.join(a[::-1])

--- ProjectMiddle/targil3.HW.py
+++ ProjectMiddle/targil3.HW.py
@@ -2,6 +2,6 @@ a=input("Enter Full name: ")
 print(' '.join(a[::-1]))
 
 
-ainshtein genius
-suineg niethsnia
-s u i n e g   n i e t h s n i a
+# ainshtein genius
+# suineg niethsnia
+# s u i n e g   n i e t h s n i a
