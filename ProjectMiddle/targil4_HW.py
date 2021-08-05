9f8f6b0c05fdbb84195de543457ba8696b85b480
lnigmail <87995463+lnigmail@users.noreply.github.com>
2021-08-05 23:38:10 +0300

    file extension

---
+++ ProjectMiddle/targil4_HW.py
@@ -0,0 +1,19 @@
+from time import sleep
+print("Now we will get details about a file name and extention...")
+for i in range (1):
+    name=input("Enter a file name: ")
+    extension=input("enter an extension: ")
+    print("Printing: " + str(i+1))
+    sleep(3)
+    print("The exteinsion of the file is: " + extension)
+print("\nAlwase a pleasure... Bye Bye... ")
+
+# Now we will get details about a file name and extention...
+# Enter a file name: abc
+# enter an extension: java
+# Printing: 1
+#
+#
+# The exteinsion of the file is: java
+#
+# Alwase a pleasure... Bye Bye...
