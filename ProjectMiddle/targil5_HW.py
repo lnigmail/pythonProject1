678cd46a24b5d8d1b590ca0caa1222dc10d781cb
lnigmail <87995463+lnigmail@users.noreply.github.com>
2021-08-06 00:49:26 +0300

    n+nn+nnn

---
+++ ProjectMiddle/targil5_HW.py
@@ -0,0 +1,20 @@
+from time import sleep
+print("Now we will accept an integer and compute the value...\n")
+for i in range (1):
+    n=input("Enter an integer: ")
+    print("\nPrinting: " + str(i+1))
+    sleep (3)
+    print("\nThe value of n+nn+nnn is: " + str(int(n) + int(n+n) + int(n+n+n)))
+
+print("\nAlwase a pleasure...Bye Bye...")
+
+# Now we will accept an integer and compute the value...
+
+# Enter an integer: 5
+
+# Printing: 1
+
+
+# The value of n+nn+nnn is: 615
+
+# Alwase a pleasure...Bye Bye...
