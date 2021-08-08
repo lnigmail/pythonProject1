71ada27d15e78ff8701f2a1351dbd2e4df7fc568
lnigmail <87995463+lnigmail@users.noreply.github.com>
2021-08-08 13:43:29 +0300

    converting feet/inches to 7 centemeters

---
+++ ProjectMiddle/targil7_HW.py
@@ -0,0 +1,33 @@
+
+num=input("Enter a number to convert height to 7 centimeters: ")
+choice=input("Enter from Menu:\n--------\n1.feet\n2.inches\n ")
+feet=(int(num))/(float(30.48))
+inches=(int(num))/(float(2.54))
+if (choice=="1"):
+    print("This feet-number in 7 centimeters is: " + str(7*feet))
+elif (choice=="2"):
+    print("This inches-number in 7 centimeters is: " + str(7*inches))
+else:
+    print("Enter only feet or inches!...")
+
+print("\nAlwase a pleasure, Bye Bye...")
+
+# Enter a number to convert height to 7 centimeters: 1
+# Enter from Menu:
+# --------
+# 1.feet
+# 2.inches
+#  1
+# This feet-number in 7 centimeters is: 0.22965879265091863
+#
+# Alwase a pleasure, Bye Bye...
+
+# Enter a number to convert height to 7 centimeters: 1
+# Enter from Menu:
+# --------
+# 1.feet
+# 2.inches
+#  2
+# This inches-number in 7 centimeters is: 2.755905511811023
+#
+# Alwase a pleasure, Bye Bye...
