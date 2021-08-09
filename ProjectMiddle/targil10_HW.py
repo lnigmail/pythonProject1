0e157fa5c0737972b0fb435ca95148a512a8e498
lnigmail <87995463+lnigmail@users.noreply.github.com>
2021-08-09 12:57:40 +0300

    creating a dictionary from string

--- ProjectMiddle/targil10_HW.py
+++ ProjectMiddle/targil10_HW.py
@@ -4,7 +4,7 @@
 # Expected output: {'N': 1, 'e': 1, 't': 2, '4': 1, 'U': 1}
 
 import json
-stringA = '{"N" : 1, "e" : 1, "t" : 2, "f" : 1, "U" : 1}'
+stringA = '{"N" : 1, "e" : 1, "t" : 2, "4" : 1, "U" : 1}'
 #Given string dictionary
 print("Given string: \n",stringA)
 #Using Json.loads()
@@ -12,7 +12,7 @@ res = json.loads(stringA)
 #Result
 print("The converted dictionary: \n",res)
 
-#  {"N" : 1, "e" : 1, "t" : 2, "f" : 1, "U" : 1}
+#  {"N" : 1, "e" : 1, "t" : 2, "4" : 1, "U" : 1}
 # The converted dictionary:
-#  {'N': 1, 'e': 1, 't': 2, 'f': 1, 'U': 1}
+#  {'N': 1, 'e': 1, 't': 2, '4': 1, 'U': 1}
 
