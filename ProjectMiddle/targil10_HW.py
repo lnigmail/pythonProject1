803c10a340be56dafa6199f36e75fff4c3f05b2f
lnigmail <87995463+lnigmail@users.noreply.github.com>
2021-08-09 12:54:49 +0300

    creating a dictionary from string

---
+++ ProjectMiddle/targil10_HW.py
@@ -0,0 +1,18 @@
+# Write a Python program to create a dictionary from a string.
+# Note: Track the count of the letters from the string.
+
+# Expected output: {'N': 1, 'e': 1, 't': 2, '4': 1, 'U': 1}
+
+import json
+stringA = '{"N" : 1, "e" : 1, "t" : 2, "f" : 1, "U" : 1}'
+#Given string dictionary
+print("Given string: \n",stringA)
+#Using Json.loads()
+res = json.loads(stringA)
+#Result
+print("The converted dictionary: \n",res)
+
+#  {"N" : 1, "e" : 1, "t" : 2, "f" : 1, "U" : 1}
+# The converted dictionary:
+#  {'N': 1, 'e': 1, 't': 2, 'f': 1, 'U': 1}
+
