8513c0a42630309bfb69beb49ef43772a3335d16
lnigmail <87995463+lnigmail@users.noreply.github.com>
2021-08-10 01:57:32 +0300

    cube game of for loop with randint

--- LAB7.py
+++ LAB7.py
@@ -22,9 +22,22 @@ for i in range(turns):
     if (cube1==cube2):
         if (cube1==6):
             prize_money=prize_money+1000
-            print("Congrats... You're won in a minion, You win the house: ")
+            print("Congrats...\n")
+            sleep(2)
+            print("You're won in a minion\n")
+            sleep(2)
+            print("You win the house: \n")
+            sleep(1)
         else:
             prize_money=prize_money+100
+            print("U\n")
+            sleep(1)
+            print("r\n")
+            sleep(1)
+            print("1\n")
+            sleep(1)
+            print("in a hundred...\n")
+            sleep(1)
     elif (cube2==2):
         prize_money=prize_money+40
     elif (cube1==1):
@@ -40,3 +53,83 @@ for i in range(turns):
     print("Your prize money is: " + str(prize_money) + " NIS\n\n")
     sleep(5)
 
+'''
+Welcome to the CUBE THRILL...
+
+Each turn cost 3 NIS (;
+
+Enter the amount of money you wish to burn...
+10
+Your change: 1 NIS
+
+You have 3 turns...
+
+
+Rolling the dice for round number 1
+----------------------------------
+
+   (: 
+        (: 
+              (: 
+
+cube1: 6
+cube2: 5
+
+Calculating the damage...
+
+
+                  (: 
+          (: 
+   (: 
+
+Your prize money is: 0 NIS
+
+
+
+
+Rolling the dice for round number 2
+----------------------------------
+
+   (: 
+        (: 
+              (: 
+
+cube1: 6
+cube2: 6
+
+Congrats...
+
+You're won in a minion
+
+You win the house: 
+
+Calculating the damage...
+
+
+                  (: 
+          (: 
+   (: 
+
+Your prize money is: 1000 NIS
+
+
+
+
+Rolling the dice for round number 3
+----------------------------------
+
+   (: 
+        (: 
+              (: 
+
+cube1: 5
+cube2: 1
+
+Calculating the damage...
+
+
+                  (: 
+          (: 
+   (: 
+
+Your prize money is: 1000 NIS'''
