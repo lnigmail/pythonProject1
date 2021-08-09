ee743b1994d36cc64f49661c73ea45b75f799d65
lnigmail <87995463+lnigmail@users.noreply.github.com>
2021-08-09 19:10:48 +0300

    sum all items in a list

---
+++ ProjectMiddle/targil13_HW.py
@@ -0,0 +1,11 @@
+#13. Write a Python program to sum all the items in a list.
+list=[50,100,150,200,250,300,350,400,450,500]
+
+sum=0
+
+for i in range(len(list)):
+    sum=sum+list[i]
+
+print(sum)
+
+#2750
