b1be9082dada9e62283f94bfde889499dfa6b230
lnigmail <87995463+lnigmail@users.noreply.github.com>
2021-08-09 16:05:30 +0300

    mix & match between 2 lists

---
+++ ProjectMiddle/targil11_HW.py
@@ -0,0 +1,10 @@
+'''11. Write a Python program to get a single string from two given strings,
+separated by a space and swap the first two characters of each string.
+Sample String : 'abc', 'xyz'
+Expected Result : 'xyc abz'''
+
+lista="abc"
+listb="xyz"
+
+print((listb[:2]+lista[2]), (lista[:2]+listb[2]))
+# xyc abz
