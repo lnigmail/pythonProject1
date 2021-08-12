bf4ff984b5c72791fd58640f7b0452ee5db5709a
lnigmail <87995463+lnigmail@users.noreply.github.com>
2021-08-13 02:15:24 +0300

    function append

--- FUN/function4.py
+++ FUN/function4.py
@@ -10,10 +10,24 @@ ip_n1=input("Enter first ip: ")
 ip_n2=input("Enter 2nd ip: ")
 ip_n3=input("Enter 3rd ip: ")
 ip_list=add_ip(ip_list,ip_n1,ip_n2,ip_n3)
-ip_list=add_ip(ip_list, ip_n1,ip_n2,ip_n3)
+# ip_list=ip_list+add_ip(ip_list,ip_n1,ip_n2,ip_n3)
 print(ip_list)
 
 # Enter first ip: 1.1.1.1
 # Enter 2nd ip: 2.2.2.2
 # Enter 3rd ip: 3.3.3.3
 # ['1.1.1.1', '2.2.2.2', '3.3.3.3']
+
+ip_list=[]
+ip_n1=input("Enter first ip: ")
+ip_n2=input("Enter 2nd ip: ")
+ip_n3=input("Enter 3rd ip: ")
+ip_list=add_ip(ip_list,ip_n1,ip_n2,ip_n3)
+ip_list=ip_list+add_ip(ip_list,ip_n1,ip_n2,ip_n3)
+print(ip_list)
+
+# Enter first ip: 1.1.1.1
+# Enter 2nd ip: 2.2.2.2
+# Enter 3rd ip: 3.3.3.3
+# ['1.1.1.1', '2.2.2.2', '3.3.3.3', '1.1.1.1', '2.2.2.2', '3.3.3.3']
+# [overwriten the line before it]
