a55a6cb8245e0ff01038ec5ee02724d765ccedcb
lnigmail <87995463+lnigmail@users.noreply.github.com>
2021-08-15 23:49:18 +0300

    LESSONs

--- EndExercise/targil5.py
+++ EndExercise/targil5.py
@@ -7,10 +7,10 @@ from random import randint
 num1=randint(1,3)
 num2=randint(1,3)
 num3=randint(1,3)
-print("Randintegers: " + "\n------------\nnum1: " + str(num1) + "\nnum2: " + str(
+print("\nRandintegers: " + "\n------------\nnum1: " + str(num1) + "\nnum2: " + str(
     num2) + "\nnum3: " + str(num3))
+sum=num1+num2+num3
+print("\nThe sum of the three given numbers: " + str(sum) + "\n")
 if (num1==num2 or num2==num3 or num1==num3):
-    sum=num1+num2+num3
-    print("\nDue to equal numbers:\n---------------------\n" 
-          "The sum of the three given numbers: " + str(sum) + "\n" +
-          "Return three times of their sum: " + str(sum*3))
+    print("Due to equal numbers:\n---------------------\n" +
+           "Return three times of their sum: " + str(sum*3))
