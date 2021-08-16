ef7fad542a7253aeaeeca53ded46072b1b7c5e4c
lnigmail <87995463+lnigmail@users.noreply.github.com>
2021-08-16 16:22:20 +0300

    LESSONs

--- EndExercise/targil6.py
+++ EndExercise/targil6.py
@@ -2,14 +2,14 @@
 Write a Python program to sum of two given integers. However, if the sum
 is between 15 to 20 it will return 20.
 '''
-from random import randint
+def sum(x, y):
+    sum = x + y
+    if sum in range(15, 20):
+        return 20
+    else:
+        return sum
 
-num1=randint(1,20)
-num2=randint(1,20)
-sum=num1+num2
-if (sum<=20 and sum>=15):
-    print("Due to sum>=15 or sum<=20 : sum=20")
-else:
-    print("\nThe sum of the randintegers: " + "\n------------\nnum1: " + str(num1) + "\nnum2: " + str(num2))
+print(sum(10, 6))
+print(sum(10, 2))
+print(sum(10, 12))
 
-print("\nAlwase a pleasure, Bye Bye...")
