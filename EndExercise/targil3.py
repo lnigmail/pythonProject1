9eab5f461de18761b8363a5efd8815791e368ebb
lnigmail <87995463+lnigmail@users.noreply.github.com>
2021-08-16 14:24:01 +0300

    LESSONs

--- EndExercise/targil3.py
+++ EndExercise/targil3.py
@@ -4,11 +4,19 @@ value of n+nn+nnn
 Sample value of n is 5
 Expected Result : 615
 '''
+#
+# print("Now we will accept an integer and compute the value...\n")
+# for i in range (1):
+#     n=input("Enter an integer: ")
+#     print("\nPrinting: " + str(i+1))
+#     print("\nThe value of n+nn+nnn is: " + str(int(n) + int(n+n) + int(n+n+n)))
+#
+# print("\nAlwase a pleasure...Bye Bye...")
 
-print("Now we will accept an integer and compute the value...\n")
-for i in range (1):
-    n=input("Enter an integer: ")
-    print("\nPrinting: " + str(i+1))
-    print("\nThe value of n+nn+nnn is: " + str(int(n) + int(n+n) + int(n+n+n)))
+n=int(input("Enter a number: "))
+print(n + (n*10+n) + (n*100+n*10+n))
 
-print("\nAlwase a pleasure...Bye Bye...")
+
+
+
+
