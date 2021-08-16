48b167523bfb77c8f64e80a084a1386288efcb19
lnigmail <87995463+lnigmail@users.noreply.github.com>
2021-08-16 14:42:51 +0300

    LESSONs

--- EndExercise/targil4.py
+++ EndExercise/targil4.py
@@ -3,11 +3,19 @@ Write a Python program to calculate number of days between two dates.
 Sample dates : (2014, 7, 2), (2014, 7, 11)
 Expected output : 9 days
 '''
-from datetime import datetime, timedelta
+'''from datetime import datetime, timedelta
 
 dt = datetime.now()
 
 print("Date and Time= " + str(dt))
 
 future_dt = dt + timedelta(days=9)
-print("Future Date and Time  = " + str(future_dt))
+print("Expected output :  = " + str(future_dt))'''
+
+from datetime import date
+f_date = date(2021, 8, 1)
+print(f_date)
+l_date = date(2021, 8, 10)
+print(l_date)
+delta=l_date-f_date
+print("Expected output : " + str(delta.days) + " days")
