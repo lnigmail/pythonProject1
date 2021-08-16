a10f68e6e813c6e9847b7b8d3b27467afdbd4a33
lnigmail <87995463+lnigmail@users.noreply.github.com>
2021-08-16 13:10:12 +0300

    LESSONs

--- EndExercise/targil1.py
+++ EndExercise/targil1.py
@@ -1,19 +1,7 @@
-'''
-Write a Python program to print the following string in a specific format (see
-the output).
-Twinkle, twinkle, little star,
+
+print('''Twinkle, twinkle, little star,
 	How I wonder what you are!
 		Up above the world so high,
 		Like a diamond in the sky.
 Twinkle, twinkle, little star,
-	How I wonder what you are
-'''
-
-def my_functions():
-    print("Twinkle, twinkle, little star,\n" +
-          "\tHow I wonder what you are!\n" +
-            "\t\tUp above the world so high,\n" +
-            "\t\tLike a diamond in the sky.\n"
-    "Twinkle, twinkle, little star,\n" +
-          "\tHow I wonder what you are")
-my_functions()
+	How I wonder what you are''')
