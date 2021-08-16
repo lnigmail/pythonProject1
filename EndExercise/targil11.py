c6344401fa6119039d6648b823fe72de863a00fb
lnigmail <87995463+lnigmail@users.noreply.github.com>
2021-08-16 18:48:28 +0300

    LESSONs

--- EndExercise/targil11.py
+++ EndExercise/targil11.py
@@ -4,11 +4,11 @@ of the cube of all the positive integers smaller than the specified number.
 '''
 
 def positive(num):
- num = num-1
+ n = n-1
  total = 0
- while num > 0:
-   total = total + (num * num * num)
-   num = num-1
+ while n > 0:
+   total = total + (n * n * n)
+   n = n-1
  return total
 print("Cube sum of smaller than specified: ",positive(3))
 
