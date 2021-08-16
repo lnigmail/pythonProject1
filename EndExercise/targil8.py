6feb42b1f0e870021b93c52292a0ed14eacf5baf
lnigmail <87995463+lnigmail@users.noreply.github.com>
2021-08-16 18:00:31 +0300

    LESSONs

--- EndExercise/targil8.py
+++ EndExercise/targil8.py
@@ -1,27 +1,9 @@
 '''
-Write a Python program to convert height (in feet and inches) to
-centimeters.
+Write a Python program to solve (x + y) * (x + y).
+Test Data : x = 4, y = 3
+Expected Output : (4 + 3) ^ 2) = 49
 '''
-def Menu():
-    while "True":
 
-        num=input("Enter a number to convert height to centimeters: ")
-        choice=input("Enter from Menu:\n---------------\n1.feet\n2.inches\n ")
-        feet=(int(num))/(float(30.48))
-        inches=(int(num))/(float(2.54))
-        if (choice=="1"):
-            print("This feet-integer in centimeters is: " + str(feet))
-        elif (choice=="2"):
-            print("This inche-integer in centimeters is: " + str(inches))
-        else:
-            print("Enter only feet or inches!")
-            continue
-
-        exit=input("\nDo you want to exit? yes/no\n")
-        if (exit=="yes"):
-            break
-        else:
-            print("Welcome back!\n")
-    print("\nAlwase a pleasure, Bye Bye...")
-
-Menu()
+x, y = 4, 3
+result = x * x + 2 * x * y + y * y
+print("({} + {}) ^ 2) = {}".format(x, y, result))
